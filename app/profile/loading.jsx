6b1bf98e92b819c1f13a430b1b5c88c3be12cc00
quangvinht <96629186+quangvinht@users.noreply.github.com>
2023-06-01 16:58:35 +0700

    fix 100

--- app/profile/loading.jsx
+++ app/profile/loading.jsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const Loading = () => {
   return (
-    <div className='w-full flex-center'>
+    <div className="w-full flex-center">
       <Image
-        src='assets/icons/loader.svg'
-        width={50}
-        height={50}
-        alt='loader'
-        className='object-contain'
+        src="assets/icons/loader.svg"
+        width={60}
+        height={60}
+        alt="loader"
+        className="object-contain"
       />
     </div>
   );
